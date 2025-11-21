f447e894d9457c0f7fa1c5448861bf21d8909c1e
Lohitha Kota <141434054+LOHITHAKOTA@users.noreply.github.com>
2025-11-21 13:38:05 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/ProjectR1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectR1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectR1Application.class, args);
+	}
+
+}
